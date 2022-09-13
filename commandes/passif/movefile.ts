34a2a7b4607656c5e23a59fa1592b9b97be809e2
KaiT0_ <68385671+ShiKaiT0@users.noreply.github.com>
2022-09-13 15:02:32 +0200

    v0.3.4d - Modifs mineures de movefile

--- commandes/passif/movefile.ts
+++ commandes/passif/movefile.ts
@@ -19,22 +19,50 @@ export default{
             description: "Raison du mouvement de fichier",
             required: true,
             type: DiscordJS.Constants.ApplicationCommandOptionTypes.STRING,
+        },
+        {
+            name: "noJustify",
+            description: "Affiche 'non spécifiée' en raison.",
+            type: DiscordJS.Constants.ApplicationCommandOptionTypes.BOOLEAN,
         }
 
     ],
 
     callback: ({interaction}) => {
+
+        /**
+         * Vérification gérer messages ici
+         */
+
+
         embed = new MessageEmbed
         .setAuthor({name: "Administration républicaine", iconURL: "https://static.wikia.nocookie.net/starwars/images/1/17/Coronet_cargo_manifest.png/revision/latest?cb=20120906220411"})
         .setColor("YELLOW")
         .setThumbnail("https://cdn-icons-png.flaticon.com/512/124/124837.png")
         .setTitle("Changement sur la base de données")
         .setDescription("Message Automatique d'Administration Système (MAAS)")
+        .setFields(
+            {
+                name: "Fichier :",
+                value: " " // id mesage 
+            },
+            {
+                name: "Par :",
+                value: " " // user qui invoque
+            },
+            {
+                name: "Raison :",
+                value: " " // arg raison, check si non add
+            }
+        )
+        
+
+        interaction.reply({
+            embeds: [embed]
+        })
 
 
-        /**
-         * à faire: vérif de permission : gérer messages
-         */
+
     }
 
 
